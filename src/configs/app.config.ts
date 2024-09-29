a5f912f773f586673dc0e673b0612805832d2a83
Son Nguyen <biem97@gmail.com>
2024-09-29 20:53:26 +0700

    Update icons

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,7 +1,7 @@
-import { PocketKnife } from 'lucide-react';
+import { PocketKnifeIcon } from 'lucide-react';
 
 export const AppConfig = {
   name: 'Dev Pocket',
   description: 'Dev Pocket - A pocket full of dev resources',
-  Icon: PocketKnife,
+  Icon: PocketKnifeIcon,
 };
